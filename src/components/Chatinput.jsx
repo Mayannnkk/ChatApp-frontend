c0e712f130ae282dad73be956c09dc136d6f7fc5
Mayannnkk <96884389+Mayannnkk@users.noreply.github.com>
2024-01-21 19:09:18 +0530

    Update Chatinput.jsx

--- src/components/Chatinput.jsx
+++ src/components/Chatinput.jsx
@@ -3,19 +3,26 @@ import styled from 'styled-components';
 import Picker from 'emoji-picker-react';
 import {IoMdSend} from 'react-icons/io';
 import {BsEmojiSmileFill} from 'react-icons/bs';
+import { addimage } from '../utils/APIRoutes';
+import axios from 'axios'
 
 export default function Chatinput({handlesendmsg}) {
 
     const[msg,setmsg]=useState([]);
-    const [showEmoji, setshowEmoji] = useState();
+    const [image, setimage] = useState([]);
 
 
-    const sendchat=(e)=>{
+    const sendchat=async(e)=>{
         e.preventDefault();
-        if(msg.length>0){
+
+        if(msg.length>0 ){
             handlesendmsg(msg);
             setmsg('')
         }
+        // if(image){
+            
+        //     handleimagepost({image})
+        // }
     }
 
     const handleemojiclick=(event,emoji)=>{
@@ -24,6 +31,11 @@ export default function Chatinput({handlesendmsg}) {
         message += event.emoji;
         setmsg(message)
         
+    }
+
+    const handleimage =(e)=>{
+        console.log(e.target.files[0]);
+        setimage(e.target.files[0])
     }
 
   return (
@@ -36,6 +48,7 @@ export default function Chatinput({handlesendmsg}) {
         </div>
         <form className='input-container' onSubmit={(e)=>sendchat(e)}>
             <input type="text" placeholder='type your message here' value={msg} onChange={(e)=>setmsg(e.target.value)}/>
+            {/* <input type="file" onChange={handleimage}/> */}
             <button className='submit'>
                 <IoMdSend/>
             </button>
@@ -51,12 +64,15 @@ background-color:#ffffff;
 align-items:center;
 padding:0.3rem;
 padding-bottom:0.2rem;
+box-shadow: 0 0px 100px 0 rgba(0, 0, 0, 0.02);
+
 
 .button-container{
     display:flex;
     align-items:center;
     color:white;
     justify-content:center;
+
     .emoji{
         position:relative;
         svg{
@@ -71,6 +87,8 @@ padding-bottom:0.2rem;
         }
         &:hover .picker{
             display:block;
+            color:#3bb99e;
+            transition: 0.2s;
         }
     }
 }
@@ -82,7 +100,7 @@ padding-bottom:0.2rem;
     gap:1rem;
     align-items:center;
     border-radius:5rem;
-    box-shadow: 0 0px 100px 0 rgba(0, 0, 0, 0.15);
+    box-shadow: 0 0px 10px 0 rgba(0, 0, 0, 0.09);
     input{
         width:90%;
         height:60%;
@@ -118,7 +136,12 @@ padding-bottom:0.2rem;
 
         &:hover{
             cursor:pointer;
+            background-color:#3bb99e;
+            transition: 0.2s;
         }
     }
+}
+@media screen and (max-width: 700px) {
+    grid-template-columns:7% 93%;
 }
 `;
